826634f54c59d3d6cea9b8c3911830daf4988ca3
mio <knightopress@gmail.com>
2022-03-08 17:08:11 -0500

    Update custom user model with correct field restrictions.

--- P2/users/models.py
+++ P2/users/models.py
@@ -1,16 +1,30 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, UserManager
 from django.core.validators import RegexValidator
 from django.db import models
 
 phone_num_validator = RegexValidator("[0-9]{3}-[0-9]{3}-[0-9]{4}")
 
 
-# Create your models here.
-class RestifyUser(AbstractUser):
+# we can't use AbstractUser bc we can't change its field props (e.g. email is not required)
+class RestifyUser(AbstractBaseUser):
+
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField()
 
     avatar = models.ImageField()
-    phone_num = models.TextField(validators=[phone_num_validator])  # textfield bc regex already checks length
+    phone_num = models.TextField(max_length=12, validators=[phone_num_validator])
 
-    USERNAME_FIELD = AbstractUser.EMAIL_FIELD
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = EMAIL_FIELD
 
-    REQUIRED_FIELDS = ["avatar", "phone_num"]  # safely drop AbstractUser.REQUIRED_FIELDS since email is USERNAME_FIELD
+    # required for reusing UserManager
+    is_staff = is_superuser = False  # we have no features that would require this functionality
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    # from AbstractUser
+    def clean(self):
+        super().clean()
+        self.__class__.objects.normalize_email(self.email)
