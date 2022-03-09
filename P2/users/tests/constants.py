1a0fe2837a4b5e5fce7edc645df86c6877bc3c37
mio <knightopress@gmail.com>
2022-03-08 21:29:00 -0500

    Add basic signup tests.

---
+++ P2/users/tests/constants.py
@@ -0,0 +1,14 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+with open("users/tests/72467059_p0.png", "rb") as avatar_img:
+    avatar = SimpleUploadedFile("avatar.png", avatar_img.read())
+
+signup_req = {
+    "first_name": "Mio",
+    "last_name": "Akiyama",
+    "email": "mio@akym.moe",
+    "phone_num": "000-000-0000",
+    "password1": "12345^ABCDE",
+    "password2": "12345^ABCDE",
+    "avatar": avatar
+}
---
+++ P2/users/tests/test_signup.py
@@ -0,0 +1,44 @@
+import copy
+import json
+import shutil
+from http import HTTPStatus
+
+from django.urls import reverse_lazy
+from rest_framework.test import APITestCase
+
+from users.tests.constants import signup_req
+
+
+class SignupTests(APITestCase):
+
+    def test_empty(self):
+
+        res = self.client.post(reverse_lazy("users_signup"))
+        self.assertEqual(res.status_code, HTTPStatus.BAD_REQUEST)
+
+        res_json: dict = json.loads(res.content)
+        self.assertEqual(len(res_json), 7)
+
+    def test_success(self):
+
+        res = self.client.post(reverse_lazy("users_signup"), signup_req)
+
+        self.assertEqual(res.status_code, HTTPStatus.CREATED)
+
+    def test_dup_email(self):
+
+        diff_email = copy.deepcopy(signup_req)
+        diff_email["email"] = "yui@hirasawa.moe"
+
+        self.client.post(reverse_lazy("users_signup"), diff_email)
+        res = self.client.post(reverse_lazy("users_signup"), diff_email)
+
+        self.assertEqual(res.status_code, HTTPStatus.BAD_REQUEST)
+
+        res_json = json.loads(res.content)
+        self.assertTrue(res_json["email"])  # make sure the error exists
+
+    @classmethod
+    def tearDownClass(cls):
+
+        shutil.rmtree("media/avatars")
