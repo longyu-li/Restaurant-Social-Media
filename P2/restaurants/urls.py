44972548a778ac9b6d8bf621f181f5740b19d245
mio <knightopress@gmail.com>
2022-03-12 16:17:11 -0500

    Add restaurant & blog actions endpoints.

--- P2/restaurants/urls.py
+++ P2/restaurants/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = [
+from django.urls import path
 
+from restaurants.views.action_views import like_restaurant, follow_restaurant, like_blog
+
+urlpatterns = [
+    path("<int:restaurant_id>/like/", like_restaurant, name="restaurants_like"),
+    path("<int:restaurant_id>/follow/", follow_restaurant, name="restaurants_follow"),
+    path("blog/<int:blog_id>/like/", like_blog, name="blog_like"),
 ]
