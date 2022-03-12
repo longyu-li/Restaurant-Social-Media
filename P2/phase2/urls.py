b1aa772a6c3aadddf3be799b228a53f8c2051352
mio <knightopress@gmail.com>
2022-03-12 13:23:08 -0500

    Include restaurant urls in project urls.

--- P2/phase2/urls.py
+++ P2/phase2/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
+    path("restaurants/", include("restaurants.urls")),
     path("notifications/", include("notifications.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
