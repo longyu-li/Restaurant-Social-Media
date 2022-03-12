975feb89313ea6a6c899451291ea924a592eb116
mio <knightopress@gmail.com>
2022-03-12 13:19:21 -0500

    Add likes & follows to restaurant model stub.

--- P2/restaurants/models.py
+++ P2/restaurants/models.py
@@ -1,7 +1,26 @@
 from django.db import models
 
 # Create your models here.
+from users.models import RestifyUser
 
 
 class Restaurant(models.Model):
-    pass
+
+    # todo: incomplete stub, add other fields + reasonable attributes (e.g. length)
+
+    name = models.CharField(max_length=150)
+
+    likes = models.ManyToManyField(RestifyUser, related_name="liked_restaurants")
+    follows = models.ManyToManyField(RestifyUser, related_name="followed_restaurants")
+
+
+class Blog(models.Model):
+
+    # todo: incomplete stub, add other fields + reasonable attributes (e.g. length)
+
+    title = models.CharField(max_length=150)
+
+    # the restaurant the blog post belongs to
+    restaurant = models.ForeignKey(to=Restaurant, on_delete=models.CASCADE)
+
+    likes = models.ManyToManyField(RestifyUser, related_name="liked_blogs")
