b9d1f1ed3216db7f27bab1cfe7e384dc92f13c1d
Longyu Li <longyu.li@mail.utoronto.ca>
2022-03-13 22:53:24 +0000

    add view for get restaurant

--- P2/restaurants/serializers.py
+++ P2/restaurants/serializers.py
@@ -5,11 +5,18 @@ from restaurants.models import Restaurant
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
-    follows = serializers.CharField(read_only=True)
-    likes = serializers.CharField(read_only=True)
+    follows = serializers.SerializerMethodField(read_only=True)
+    likes = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Restaurant
         fields = ['name', 'street', 'city', 'province', 'postal_code', 'logo', 'phone_num', 'banner', 'description', 'follows', 'likes']
 
+    def get_follows(self, obj):
+        return obj.follows.count()
 
+    def get_likes(self, obj):
+        return obj.likes.count()
+    
+
+    
--- P2/restaurants/urls.py
+++ P2/restaurants/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from restaurants.views.create_update_restaurant_view import CreateUpdateRestaurantView
+from restaurants.views.get_restaurant_view import GetRestaurantView
 
 from restaurants.views.action_views import like_restaurant, follow_restaurant, like_blog
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path("<int:restaurant_id>/follow/", follow_restaurant, name="restaurants_follow"),
     path("blog/<int:blog_id>/like/", like_blog, name="blog_like"),
     path("", CreateUpdateRestaurantView.as_view(), name="create_update_restaurant"),
+    path("<int:restaurant_id>/", GetRestaurantView.as_view(), name="get_restaurant"),
 ]
