735c903c7a043cdd58601fca08a98f68738a1317
George Jeffrey Medcalf Lewis <lewisge2@dh2020pc16.utm.utoronto.ca>
2022-03-15 14:51:56 -0400

    initial testing stuff

---
+++ P2/notifications/tests/test_notifications.py
@@ -0,0 +1,100 @@
+from http import HTTPStatus
+
+from django.urls import reverse_lazy
+
+from shared.authed_test import AuthedAPITestCase
+from shared.constants import get_signup_req, signin_req, get_restaurant_req
+
+
+class NotificationTests(AuthedAPITestCase):
+
+    def setUp(self) -> None:
+
+        acc1_res = self.client.post(reverse_lazy("users_signup"), get_signup_req())
+        self.assertEqual(acc1_res.status_code, HTTPStatus.CREATED)
+
+        acc2 = get_signup_req()
+        acc2["first_name"] = "Yui"
+        acc2["last_name"] = "Hirasawa"
+        acc2["email"] = "yui@hrsw.moe"
+
+        self.acc2 = acc2
+
+        acc2_res = self.client.post(reverse_lazy("users_signup"), acc2)
+        self.assertEqual(acc2_res.status_code, HTTPStatus.CREATED)
+
+        self.login(signin_req)
+
+        self.restaurant_res = self.client.post(reverse_lazy("edit_restaurant"), get_restaurant_req())
+        self.assertEqual(self.restaurant_res.status_code, HTTPStatus.CREATED)
+
+    def test_notifications(self):
+
+        # Login as user 1
+        self.login(get_signup_req())
+
+        r_id = self.restaurant_res.json()["id"]
+
+        kwargs = {"restaurant_id": r_id}
+
+        res = self.client.post(reverse_lazy("restaurants_like", kwargs=kwargs))
+        self.assertEqual(res.status_code, HTTPStatus.OK)
+
+        self.login(self.acc2)
+        res = self.client.get(reverse_lazy("notifications"))
+        self.assertEqual(res.status_code, HTTPStatus.OK)
+
+        notifs = res.json()
+
+        self.assertEqual(notifs, [
+            {
+                "type": "like",
+                "...": ...
+            }
+        ])
+
+    def test_404(self):
+
+        self.login({"email": "yui@hrsw.moe", "password": signin_req["password"]})
+
+        kwargs = {"restaurant_id": 44}
+
+        like_res = self.client.post(reverse_lazy("restaurants_like", kwargs=kwargs))
+        self.assertEqual(like_res.status_code, HTTPStatus.NOT_FOUND)
+
+        follow_res = self.client.post(reverse_lazy("restaurants_follow", kwargs=kwargs))
+        self.assertEqual(follow_res.status_code, HTTPStatus.NOT_FOUND)
+
+        # like blog test here
+
+    def test_no_self(self):
+
+        self.login(signin_req)
+
+        kwargs = {"restaurant_id": 1}
+
+        like_res = self.client.post(reverse_lazy("restaurants_like", kwargs=kwargs))
+        self.assertEqual(like_res.status_code, HTTPStatus.BAD_REQUEST)
+
+        follow_res = self.client.post(reverse_lazy("restaurants_follow", kwargs=kwargs))
+        self.assertEqual(follow_res.status_code, HTTPStatus.BAD_REQUEST)
+
+    def test_success(self):
+
+        self.login({"email": "yui@hrsw.moe", "password": signin_req["password"]})
+
+        kwargs = {"restaurant_id": 1}
+
+        like_res = self.client.post(reverse_lazy("restaurants_like", kwargs=kwargs))
+        self.assertEqual(like_res.status_code, HTTPStatus.OK)
+
+        get_like = self.client.get(reverse_lazy("restaurants_like", kwargs=kwargs))
+        self.assertEqual(get_like.status_code, HTTPStatus.OK)
+        self.assertEqual(get_like.content.decode("utf-8"), "true")
+
+        follow_res = self.client.post(reverse_lazy("restaurants_follow", kwargs=kwargs))
+        self.assertEqual(follow_res.status_code, HTTPStatus.OK)
+
+        get_follow = self.client.get(reverse_lazy("restaurants_follow", kwargs=kwargs))
+        self.assertEqual(get_follow.status_code, HTTPStatus.OK)
+        self.assertEqual(get_follow.content.decode("utf-8"), "true")
