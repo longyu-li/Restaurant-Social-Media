b8e64a8cc5558a364a49b4b7ad77611faf047186
Longyu Li <longyu.li@mail.utoronto.ca>
2022-03-16 01:38:19 +0000

    add restaurant delete endpoint

--- P2/restaurants/views/create_update_restaurant_view.py
+++ P2/restaurants/views/create_update_restaurant_view.py
@@ -1,12 +1,12 @@
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from restaurants.serializers import RestaurantSerializer
 from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 
-class CreateUpdateRestaurantView(CreateAPIView, RetrieveUpdateAPIView):
+class CreateUpdateRestaurantView(CreateAPIView, RetrieveUpdateDestroyAPIView):
 
     serializer_class = RestaurantSerializer
     permission_classes = [IsAuthenticated]
