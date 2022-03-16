fd34ae8f0583d903a89a36da36abb158b29ee3ee
Longyu Li <longyu.li@mail.utoronto.ca>
2022-03-16 01:24:25 +0000

    return 403 instead of 500 when adding more than 1 restaurant

--- P2/restaurants/views/create_update_restaurant_view.py
+++ P2/restaurants/views/create_update_restaurant_view.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 from restaurants.serializers import RestaurantSerializer
 from rest_framework.permissions import IsAuthenticated
@@ -11,7 +12,11 @@ class CreateUpdateRestaurantView(CreateAPIView, RetrieveUpdateAPIView):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        try:
+            serializer.save(user=self.request.user)
+        except:
+            raise PermissionDenied
 
     def get_object(self):
         return self.request.user.restaurant
+
