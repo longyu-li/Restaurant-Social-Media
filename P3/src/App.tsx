2c2faaa66e6662d8369dd39671d52abdca5fba19
mio <knightopress@gmail.com>
2022-04-06 23:24:40 -0400

    Add react-bootstrap.

--- P3/src/App.tsx
+++ P3/src/App.tsx
@@ -4,6 +4,7 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 
