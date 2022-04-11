8c615655ab3ef777671992fe03b0799397665085
mio <knightopress@gmail.com>
2022-04-11 12:43:21 -0400

    Fix DevTools warning.

--- P3/src/index.tsx
+++ P3/src/index.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root')!;
-createRoot(container).render(<App />);
+createRoot(container).render(<React.StrictMode><App /></React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
