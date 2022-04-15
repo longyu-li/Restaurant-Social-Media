f8f5e0997c4e51791d6f7a24da83c69d3edda1c4
mio <knightopress@gmail.com>
2022-04-15 17:51:39 -0400

    Fetch restaurant data on restaurant page.

--- P3/src/pages/Restaurant.tsx
+++ P3/src/pages/Restaurant.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import RestaurantBanner from "../components/RestaurantBanner";
 import {RestaurantProvider} from "../contexts/RestaurantContext";
 import RestaurantNav from "../components/RestaurantNav";
+import {useParams} from "react-router-dom";
 
 
 const Restaurant: React.VFC = () => {
+
+  const params = useParams();
+
+  const [restaurant, setRestaurant] = useState();
+
+  useEffect(() => {
+    fetch(`/restaurants/${params.id}`)
+      .then(res => {
+         if (res.ok) {
+
+           res.json().then(data => setRestaurant(data));
+
+         }
+      });
+  }, [params.id]);
+
+  useEffect(() => {
+    if (restaurant) {
+      console.log(restaurant);
+    }
+  }, [restaurant]);
+
   return (
       <RestaurantProvider>
           <Container fluid>
