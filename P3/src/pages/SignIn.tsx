f1c18e1cdf745d2e42ce69d2613b126483ab454c
mio <knightopress@gmail.com>
2022-04-15 18:08:08 -0400

    Modify page title appropriately.

--- P3/src/pages/SignIn.tsx
+++ P3/src/pages/SignIn.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import SignInForm from "../components/Forms/SignInForm";
 
 const SignIn: React.VFC = () => {
+
+  useEffect(() => {
+    document.title = "Restify - Sign In";
+  }, []);
+
   return (
     <Container fluid className="flex-grow-1 d-flex flex-column justify-content-center">
       <Row className="justify-content-center">
