4bb7b037e9c7e5de4790b4f75e63afeec3e1552a
George Lewis <33588728+George-lewis@users.noreply.github.com>
2022-04-18 01:03:52 -0400

    fix card

--- P3/src/components/RestaurantCard/RestaurantCard.tsx
+++ P3/src/components/RestaurantCard/RestaurantCard.tsx
@@ -19,14 +19,22 @@ const RestaurantCard: React.VFC<Props> = ({ data }) => {
         navigate(`/restaurant/${data.id}/`)
     }
 
-    const likes = data.likes ? <p><b>{data.likes}</b> likes</p> : <></>;
+    const likes = data.likes ? <span style={{fontWeight: "bold"}}>{data.likes} {data.likes === 1 ? "like" : "likes"}</span> : <></>;
+    const follows = data.follows ? <span style={{fontWeight: "bold"}}>{data.follows} {data.follows === 1 ? "follower" : "followers"}</span> : <></>;
 
     return <Card className={styles.card}>
             <a className="stretched-link" onClick={link} href=""></a>
             <Card.Img variant="top" src={ data.logo } className={styles.img}/>
             <Card.Body className={styles.body}>
                 <Card.Title className={styles.title}>{ data.name }</Card.Title>
-                {likes}
+                <div style={{
+                    display: "flex",
+                    gap: "4px"
+                }}>
+                    {likes}
+                    <b>|</b>
+                    {follows}
+                </div>
                 <span style={{
                     overflow: 'hidden',
                 }}>{data.description}</span>
