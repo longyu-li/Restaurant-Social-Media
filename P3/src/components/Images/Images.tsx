0adec3ca39a153906d73b014eaae8a72dea3ca7a
George Lewis <33588728+George-lewis@users.noreply.github.com>
2022-04-18 03:26:24 -0400

    style

--- P3/src/components/Images/Images.tsx
+++ P3/src/components/Images/Images.tsx
@@ -48,7 +48,10 @@ const Images: React.VFC<Props> = (data) => {
         >
         <Row xs={1} md={4} className="g-4">
                 {data.images.map((item) => {
-                    return <Col key={item.id}><Card >
+                    return <Col key={item.id}><Card style={{
+                        borderColor: "black",
+                        borderRadius: "6px"
+                    }}>
                         <Card.Img variant="top" src={item.image}/>
                         <Card.Body>
                             <Card.Title>{item.title}</Card.Title>
